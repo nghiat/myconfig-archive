18a2aa0b54462be886d71ffb5207045030098a05
nghiat <trantuannghia95@gmail.com>
2017-01-04 00:05:56 +0700

    Add fish config to autostartx and change some bug

--- dotfiles/conf.sh
+++ dotfiles/conf.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 mkdir -p $HOME/.config/{i3,dunst,rofi}
 ln -s $PWD/dunstrc $HOME/.config/dunst
 ln -s $PWD/{i3blocks.conf,i3lock-blur.sh} $HOME/.config/i3
 ln -s $PWD/i3-config $HOME/.config/i3/config
 ln -s $PWD/rofi-config $HOME/.config/rofi/config
 ln -s $PWD/{.xinitrc,.xserverrc} $HOME
+ln -s $PWD/config.fish $HOME/.config/fish
 sudo ln -s $PWD/i3lock-blur.service /etc/systemd/system/
 sudo systemctl enable i3lock-blur
