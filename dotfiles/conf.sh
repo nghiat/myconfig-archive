913e7a261838aeef01852c84b7afed090e018e06
nghiat <trantuannghia95@gmail.com>
2017-01-05 15:38:23 +0700

    Add xmodmap and xcape config for swapping esc, caps, and super keys

--- dotfiles/conf.sh
+++ dotfiles/conf.sh
@@ -6,5 +6,6 @@ ln -s $PWD/i3-config $HOME/.config/i3/config
 ln -s $PWD/rofi-config $HOME/.config/rofi/config
 ln -s $PWD/{.xinitrc,.xserverrc} $HOME
 ln -s $PWD/config.fish $HOME/.config/fish
+ln -s $PWD/.Xmodmap $HOME
 sudo ln -s $PWD/i3lock-blur.service /etc/systemd/system/
 sudo systemctl enable i3lock-blur
