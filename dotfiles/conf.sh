3ce9789f64d78181fefc5e36fbb131cbd4a2faad
nghiat <trantuannghia95@gmail.com>
2017-06-13 22:31:34 +0700

    Fix fish symbolic link

--- dotfiles/conf.sh
+++ dotfiles/conf.sh
@@ -5,7 +5,7 @@ ln -s $PWD/{i3blocks.conf,i3lock-blur.sh} $HOME/.config/i3
 ln -s $PWD/i3-config $HOME/.config/i3/config
 ln -s $PWD/rofi-config $HOME/.config/rofi/config
 ln -s $PWD/{.xinitrc,.xserverrc} $HOME
-ln -s $PWD/config.fish $HOME/.config/fish
+ln -s $PWD/config.fish $HOME/.config/fish/config.fish
 ln -s $PWD/.Xmodmap $HOME
 sudo ln -s $PWD/i3lock-blur.service /etc/systemd/system/
 sudo systemctl enable i3lock-blur
