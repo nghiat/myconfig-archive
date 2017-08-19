fa8d6ad947a9a9ba2e035e8a70073c1b1530eec5
nghiat <trantuannghia95@gmail.com>
2017-08-19 20:46:44 +0700

    Add i3-gaps config Change colorscheme to gruvbox

--- dotfiles/conf.sh
+++ dotfiles/conf.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 mkdir -p $HOME/.config/{i3,dunst,rofi,fish}
+if [ ! -d $HOME/.config/xfce4/terminal/colorschemes]; then
+  mkdir -p $HOME/.config/xfce4/terminal/colorschemes
+fi
 ln -s $PWD/dunstrc $HOME/.config/dunst
 ln -s $PWD/{i3blocks.conf,i3lock-blur.sh} $HOME/.config/i3
 ln -s $PWD/i3-config $HOME/.config/i3/config
@@ -8,5 +11,6 @@ ln -s $PWD/rofi-config $HOME/.config/rofi/config
 ln -s $PWD/{.xinitrc,.xserverrc} $HOME
 ln -s $PWD/config.fish $HOME/.config/fish/config.fish
 ln -s $PWD/.Xmodmap $HOME
+ln -s $PWD/gruvbox.theme $HOME/.config/xfce4/terminal/colorschemes
 sudo ln -s $PWD/i3lock-blur.service /etc/systemd/system/
 sudo systemctl enable i3lock-blur
