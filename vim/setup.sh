755c7bd482fe95a4f180e80541d50cbbfdff1ab2
Tran Tuan Nghia <trantuannghia95@gmail.com>
2017-08-25 09:19:02 +0700

    Add ultisnips and cpp snippets

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 ln -s $PWD/vimrc $HOME/.vimrc
-if [ ! -d $HOME/.vim/autoload ]; then
-  mkdir -p $HOME/.vim/autoload
-fi
-ln -s $PWD/ftplugin $HOME/.vim/ftplugin
-ln -s $PWD/after $HOME/.vim/after
-ln -s $PWD/plug.vim $HOME/.vim/autoload/plug.vim
+ln -s $PWD/vimfiles $HOME/.vim
