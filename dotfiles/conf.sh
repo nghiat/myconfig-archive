9a64a20b21d6fb661502edee692d016b51ef2562
nghiat <trantuannghia95@gmail.com>
2017-10-26 03:13:10 +0700

    Vim tmux color

--- dotfiles/conf.sh
+++ dotfiles/conf.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-mkdir -p $HOME/.config/{i3,dunst,rofi,fish}
+mkdir -p $HOME/.config/{i3,dunst,rofi,fish,termite}
 ln -s $PWD/dunstrc $HOME/.config/dunst
 ln -s $PWD/{i3blocks.conf,i3lock-blur.sh} $HOME/.config/i3
 ln -s $PWD/i3-config $HOME/.config/i3/config
 ln -s $PWD/temperature $HOME/.config/i3/temperature
 ln -s $PWD/rofi-config $HOME/.config/rofi/config
 ln -s $PWD/config.fish $HOME/.config/fish/config.fish
+ln -s $PWD/termite-config $HOME/.config/termite/config
 ln -s $PWD/{.Xmodmap,.tmux.conf,.Xresources,gruvbox-dark.xresources,.xinitrc,.xserverrc} $HOME
 sudo ln -s $PWD/i3lock-blur.service /etc/systemd/system/
 sudo systemctl enable i3lock-blur
