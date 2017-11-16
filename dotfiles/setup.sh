472c7fcbb736f61b7bd56f6326535669f694f556
nghiat <trantuannghia95@gmail.com>
2017-11-17 05:34:41 +0700

    Readd gruvbox scheme for xresources

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -29,6 +29,8 @@ declare -A symlink=(
     ["temperature"]="${dirs[i3]}"
     [".xinitrc"]="$HOME"
     [".xserverrc"]="$HOME"
+    [".Xresources"]="$HOME"
+    ["gruvbox-dark.xresources"]="$HOME"
 )
 
 declare -A sudo_symlink=(
@@ -88,6 +90,7 @@ setup() {
         create_symlink_from_array "true" "laptop_sudo_symlink"
     fi
     sudo sed -i 's/#HandleLidSwitch=suspend.*/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+    sudo sed -i 's/#HandlePowerKey.*/HandlePowerKey=suspend/' /etc/systemd/logind.conf
     sudo sed -i 's/#DefaultTimeoutStartSec.*/DefaultTimeoutStartSec=10s/' /etc/systemd/system.conf
     sudo sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=10s/' /etc/systemd/system.conf
     sudo systemctl daemon-reload
