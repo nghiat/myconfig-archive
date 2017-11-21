af0c51ea9fd4d3ae1d80e051d4649f5ecfbc9088
nghiat <trantuannghia95@gmail.com>
2017-11-22 04:17:57 +0700

    Add more emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -23,9 +23,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((((class color) (min-colors 16777215)) (:background "#282828" :foreground "#fdf4c1")) (((class color) (min-colors 255)) (:background "#262626" :foreground "#ffffaf")))))
 
 (setq use-package-always-ensure t)
 (use-package magit)
 (use-package evil)
+(use-package gruvbox-theme)
+(use-package flycheck)
+(use-package ranger)
+(use-package company)
+
+(add-hook 'after-init-hook 'global-company-mode)
 (evil-mode 1)
+(load-theme 'gruvbox-dark-hard t)
+(let ((frame-background-mode 'light)) (frame-set-background-mode nil))
+(if (eq system-type 'windows-nt)
+    (set-frame-font "Consolas Bold 12" nil t)
+(set-frame-font "Dejavu Sans Mono Bold 11" nil t))
+(global-linum-mode t)
+(savehist-mode 1)
+(setq x-select-enable-clipboard t)
+(global-set-key (kbd "<f7>") 'ranger)
