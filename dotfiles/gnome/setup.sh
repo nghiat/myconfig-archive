407bc42a902e1d89e2f03ffc63f45a9146e66fac
nghiat <trantuannghia95@gmail.com>
2017-11-22 02:41:52 +0700

    Add more packages to lists

--- dotfiles/gnome/setup.sh
+++ dotfiles/gnome/setup.sh
@@ -2,5 +2,6 @@
 script_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 source $script_dir/../export_vars.sh
 source $script_dir/../utils/parse_args.sh "$@"
-echo -e "${Red}gnome-control-center gnome-tweak-tool gnome-shell gdm${Nc}"
+echo -e "${Red}gnome-control-center gnome-tweak-tool gnome-shell gdm chrome-gnome-shell-git${Nc}"
+echo -e "${Red}dconf dump /org/gnome/ > backup.txt${Nc}"
 echo -e "${Red}dconf load /org/gnome/ < backup.txt${Nc}"
