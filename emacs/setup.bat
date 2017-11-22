538219b040676831ba98bc5f44c02d87e5b0b215
nghiat <trantuannghia95@gmail.com>
2017-11-22 20:00:42 +0700

    Add more .emacs.d symlink on Windows

--- emacs/setup.bat
+++ emacs/setup.bat
@@ -1,8 +1,10 @@
 @echo off
 if exist %USERPROFILE%\AppData\Roaming\.emacs.d (
     rd /S /Q %USERPROFILE%\AppData\Roaming\.emacs.d
-) else (
-    echo a
 )
-
 mklink /D %USERPROFILE%\AppData\Roaming\.emacs.d %USERPROFILE%\myconfig\emacs\.emacs.d
+
+if exist %USERPROFILE%\.emacs.d (
+    rd /S /Q %USERPROFILE%\.emacs.d
+)
+mklink /D %USERPROFILE%\.emacs.d %USERPROFILE%\myconfig\emacs\.emacs.d
