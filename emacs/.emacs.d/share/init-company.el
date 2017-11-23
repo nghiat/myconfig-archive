9ea8d43857a3563255020af6c2adff08689d115e
Tran Tuan Nghia <trantuannghia95@gmail.com>
2017-11-23 10:53:35 +0700

    Customize company settings

--- emacs/.emacs.d/share/init-company.el
+++ emacs/.emacs.d/share/init-company.el
@@ -1,2 +1,8 @@
 (add-hook 'after-init-hook 'global-company-mode)
+(setq company-idle-delay 0.1)
+(setq company-minimum-prefix-length 2)
+(setq company-show-numbers t)
+(define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
+(define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort)
+
 (provide 'init-company)
