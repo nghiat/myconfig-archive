4f198002d9276bb61cbf9492a64e11af91175a0e
nghiat <trantuannghia95@gmail.com>
2018-01-21 20:34:38 +0700

    Update config for fish shell

--- setup.sh
+++ setup.sh
@@ -6,7 +6,11 @@ fi
 ln -s $PWD/emacs/.emacs.d ~
 
 ln -s $PWD/emacs/.globalrc ~
-export GTAGSCONF="$HOME/myconfig/.globalrc"
+if [[ "$SHELL" =~ "bash" ]]; then
+    export GTAGSCONF="$HOME/myconfig/.globalrc"
+elif [[ "$SHELL" =~ "fish" ]]; then
+    fish -c "set -gx GTAGGCONF $HOME/myconfig/.globalrc"
+fi
 
 #vim
 ln -s $PWD/vim/vimrc $HOME/.vimrc
