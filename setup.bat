b884e2637b64b2e6ce7a5fbb6006ac65967aa962
nghiat <trantuannghia95@gmail.com>
2018-01-21 17:14:49 +0000

    Add cygwin color scheme

--- setup.bat
+++ setup.bat
@@ -16,3 +16,6 @@ setx GTAGSCONF "%~dp0\emacs\.globalrc"
 rem vim
 mklink %USERPROFILE%\_vimrc %~dp0\vim\vimrc
 mklink /D %USERPROFILE%\vimfiles %~dp0\vim\vimfiles
+
+rem cygwin
+mklink %USERPROFILE%\.minttyrc %~dp0\windows\.minttyrc
