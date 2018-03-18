6d9463e99960b31da7d82b260d148bb6cdecce9d
nghiat <trantuannghia95@gmail.com>
2018-03-18 11:51:04 +0700

    gnome: Update comment

--- gnome/gnome.py
+++ gnome/gnome.py
@@ -1,5 +1,3 @@
-note = """gnome-control-center gnome-tweak-tool
-gnome-shell gdm chrome-gnome-shell-git gnome-system-monitor
-dconf dump /org/gnome/ > backup.txt
-dconf load /org/gnome/ < backup.txt
+note = """gnome-control-center gnome-tweak-tool gnome-shell gdm chrome-gnome-shell-git
+extensions: Alternatetab, Launch new instance, Media player indicator, Removable drive menu, Topicons plus
 """
