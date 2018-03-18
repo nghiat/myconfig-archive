b148f8933c949c795d292e092e4cedeed380f18b
nghiat <trantuannghia95@gmail.com>
2018-03-18 11:26:28 +0700

    emacs: Add copy init-local.el

--- emacs/emacs.py
+++ emacs/emacs.py
@@ -1,4 +1,7 @@
 from scripts.envvar import set_envvar
+from scripts.subprocess_wrapper import run_command
+import os
+
 links = {
     "~/.emacs.d": ".emacs.d",
     "~/.globalrc": ".globalrc"
@@ -9,3 +12,9 @@ note = "pip install jedi flake8 autopep8"
 
 def setup():
     set_envvar("GTAGSCONF", ".globalrc")
+    if os.name == "nt":
+        run_command(
+            "copy .emacs.d/init.d/init-local.el.template .emacs.d/init.d/init-local.el")
+    else:
+        run_command(
+            "cp .emacs.d/init.d/init-local.el.template .emacs.d/init.d/init-local.el")
