335e22031541273a27400a2ebf681ad7fba8cec0
nghiat <trantuannghia95@gmail.com>
2018-04-12 17:07:14 +0700

    vscode: Update settings

--- vscode/vscode.py
+++ vscode/vscode.py
@@ -1,5 +1,16 @@
 note = """
-"editor.tokenColorCustomizations": {
+{
+  "editor.fontSize": 15,
+  "editor.fontFamily": "Consolas, 'DejaVu Sans Mono'",
+  "editor.detectIndentation": false,
+  "editor.insertSpaces": true,
+  "editor.tabSize": 2,
+  // Add a line at 80 characters.
+  "editor.rulers": [
+    80
+  ],
+  "workbench.colorTheme": "Default Light+",
+  "editor.tokenColorCustomizations": {
     "functions": "#000000",
     "comments": "#880000",
     "strings": "#008800",
@@ -7,5 +18,15 @@ note = """
     "numbers": "#000000",
     "types": "#000000",
     "variables": "#000000"
+  },
+  "workbench.startupEditor": "newUntitledFile",
+  "workbench.iconTheme": "vscode-icons",
+
+  // Vim
+  "vim.hlsearch": true,
+  "vim.ignorecase": false,
+  "vim.leader": "<space>",
+  "vim.useSystemClipboard": true,
+  "vim.visualstar": true,
 }
 """
