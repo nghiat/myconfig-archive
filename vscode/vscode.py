0c84135d9e8dd80ce5ca919abc8d91ff5cb986c2
nghiat <trantuannghia95@gmail.com>
2018-04-20 18:08:11 +0700

    vscode: Proper config

--- vscode/vscode.py
+++ vscode/vscode.py
@@ -1,32 +1,42 @@
-note = """
-{
-  "editor.fontSize": 15,
-  "editor.fontFamily": "Consolas, 'DejaVu Sans Mono'",
-  "editor.detectIndentation": false,
-  "editor.insertSpaces": true,
-  "editor.tabSize": 2,
-  // Add a line at 80 characters.
-  "editor.rulers": [
-    80
-  ],
-  "workbench.colorTheme": "Default Light+",
-  "editor.tokenColorCustomizations": {
-    "functions": "#000000",
-    "comments": "#880000",
-    "strings": "#008800",
-    "keywords": "#000088",
-    "numbers": "#000000",
-    "types": "#000000",
-    "variables": "#000000"
-  },
-  "workbench.startupEditor": "newUntitledFile",
-  "workbench.iconTheme": "vscode-icons",
+import os
+import platform
+from scripts.subprocess_wrapper import run_command
 
-  // Vim
-  "vim.hlsearch": true,
-  "vim.ignorecase": false,
-  "vim.leader": "<space>",
-  "vim.useSystemClipboard": true,
-  "vim.visualstar": true,
-}
-"""
+extension_ids = [
+    "christian-kohler.path-intellisense",
+    "donjayamanne.githistory",
+    "HookyQR.beautify",
+    "ms-python.python",
+    "ms-vscode.cpptools",
+    "npclaudiu.vscode-gn",
+    "robertohuertasm.vscode-icons",
+    "vscodevim.vim",
+    "xaver.clang-format",
+]
+
+files = ["keybindings.json", "settings.json"]
+
+
+def create_links(prefix_path):
+    results = {}
+    for filename in files:
+        results[os.path.join(prefix_path, filename)] = filename
+    return results
+
+
+if platform.system() == "Windows":
+    links = create_links("~/AppData/Roaming/Code/User")
+elif platform.system() == "Linux":
+    links = create_links("~/.config/Code/User")
+elif platform.system() == "Darwin":
+    links = create_links("~/Library/Application Support/Code/User")
+
+
+def setup():
+    for id in extension_ids:
+        run_command("code --install-extension " + id)
+
+
+def clean():
+    for id in extension_ids:
+        run_command("code --uninstall-extension " + id)
