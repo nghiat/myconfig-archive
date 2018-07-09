565657e8684175a5a9ccf8ba2f9d0ca80d0c3cd9
nghiat <trantuannghia95@gmail.com>
2018-07-09 14:56:48 +0700

    linux-general: Switch from zsh to bash. vim: Removes youcompleteme.

---
+++ linux-general/.bashrc
@@ -0,0 +1,8 @@
+export PATH="/bin:/usr/bin:$PATH";
+
+# Autocorrect typos when using cd
+shopt -s cdspell;
+# Avoid duplicates
+export HISTCONTROL=ignoredups:erasedups
+# Append history
+shopt -s histappend
--- linux-general/linux-general.py
+++ linux-general/linux-general.py
@@ -7,6 +7,8 @@ links = {
     "/etc/X11/xorg.conf.d/00-custom-kdb.conf": "00-custom-kdb.conf",
     "~/.zshrc": ".zshrc",
     "~/.Xdefaults": ".Xdefaults",
+    "~/.bashrc": ".bashrc",
+    "~/.inputrc": ".inputrc",
 }
 
 note = '''zsh tmux xsel urxvt redshift-gtk python-xdg
@@ -17,9 +19,9 @@ setxkbmap -option "ctrl:nocaps"'''
 
 def setup():
     run_script("change-power-settings.sh")
-    run_command("git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh")
-    run_command("git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions")
-    run_command("sed -i 's/%c/%d/g' ~/.oh-my-zsh/themes/robbyrussell.zsh-theme")
+    # run_command("git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh")
+    # run_command("git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions")
+    # run_command("sed -i 's/%c/%d/g' ~/.oh-my-zsh/themes/robbyrussell.zsh-theme")
     run_command("dconf load /apps/guake/ < ~/myconfig/linux-general/guake.conf")
     run_command("git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm")
     run_command("echo kernel.yama.ptrace_scope=0 | sudo tee /etc/sysctl.d/10-ptrace.conf > /dev/null")
