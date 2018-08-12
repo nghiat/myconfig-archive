4970dfee313b8473b4c117acfa6e8206a1745f1f
nghiat <trantuannghia95@gmail.com>
2018-08-12 10:34:06 +0700

    i3: Remove all theme   Remove i3lock-blur   Remove i3blocks   Remove polybar   Remove rofi   Simplify config

--- i3/i3.py
+++ i3/i3.py
@@ -1,18 +1,31 @@
+from scripts.subprocess_wrapper import run_command
+import getpass
+
 links = {
     "~/.xinitrc": ".xinitrc",
     "~/.xserverrc": ".xserverrc",
-    "~/.Xresources": ".Xresources",
-    "~/solarized-light.xresources": "solarized-light.xresources",
-    "~/gruvbox-dark.xresources": "gruvbox-dark.xresources",
     "~/.config/dunst/dunstrc": "dunstrc",
     "~/.config/i3/config": "i3",
-    "~/.config/i3/i3blocks.conf": "i3blocks.conf",
-    "~/.config/polybar/config": "polybar",
-    "~/.config/rofi/config": "rofi",
-    "/etc/systemd/system/i3lock-blur.service": "i3lock-blur.service"
+    "~/.config/i3/i3status.conf": "i3status.conf",
+    "/etc/systemd/system/i3lock@.service": "i3lock@.service",
 }
 
-note = """fish network-manager-applet compton dropbox redshift gnome-keyring
-fsearch-git dunst i3blocks i3lock-blur rofi xorg-xset xautolock ibus
-ibus-unikey ttf-font-awesome ttf-dejavu ranger polybar w3m xf86-input-synaptics
-acpid python-xdg xsel xorg-xbacklight pulseaudio arandr"""
+def setup():
+    run_command("sudo systemctl enable i3lock@{}.service".format(getpass.getuser()))
+
+note = """
+network-manager-applet
+compton
+gnome-keyring
+dunst
+rofi
+xorg-xset
+xautolock
+ttf-dejavu
+pasystray
+xf86-input-synaptics
+acpid
+xsel
+xorg-xbacklight
+pulseaudio
+arandr"""
--- i3/i3lock-blur.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-TMPBG=/tmp/screen.png
-RES=1920x1080
-ffmpeg -f x11grab -video_size $RES -i $DISPLAY -filter_complex "boxblur=20:1" -vframes 1 $TMPBG -loglevel quiet
-i3lock -i $TMPBG
-rm $TMPBG
